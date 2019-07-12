5ef6ffb4c96eb7319ca884a49078a3419dc07643
pendyalasasidhar <49013577+pendyalasasidhar@users.noreply.github.com>
2019-07-12 16:36:49 +0530

    Add files via upload

---
+++ Controllers/homeController.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class homeController
+        Inherits Controller
+
+        ' GET: home
+        Function Index() As ActionResult
+            Return View()
+        End Function
+        Function Getdata() As ActionResult
+            Return View()
+        End Function
+        Function contact() As ActionResult
+            Return View()
+        End Function
+
+    End Class
+End Namespace
